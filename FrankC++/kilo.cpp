f2f295a45b195fbefb07f29d47ead1405973df46
Jose Servin <Servinjose96@gmail.com>
2022-09-11 17:38:23 -0500

    new test files added

---
+++ FrankC++/kilo.cpp
@@ -0,0 +1,10 @@
+// This program will take in kilometers and return back miles.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
---
+++ FrankC++/semiprob.cpp
@@ -0,0 +1,12 @@
+// This program demonstrates a compile error.
+
+// Place your name here
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	return 0;
+}
