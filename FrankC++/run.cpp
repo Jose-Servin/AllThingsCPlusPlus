7a5d98c7b414cd2f3a72d6486547531f08224d86
Jose Servin <Servinjose96@gmail.com>
2022-09-14 12:10:00 -0500

    new compile instructions and continued notes on vectors and dynamic vector usage

--- FrankC++/run.cpp
+++ FrankC++/run.cpp
@@ -5,7 +5,30 @@ using namespace std;
 int main()
 {
 
-    std::vector<int> test_scores = {78, 99, 85, 90, 60};
+    vector<int> test_scores = {90, 86, 99, 79, 88};
+
+    cout << "Currently we have: " << test_scores.size() << " elements in our vector" << endl;
+    cout << test_scores.at(0) << endl;
+    cout << test_scores.at(1) << endl;
+    cout << test_scores.at(2) << endl;
+    cout << test_scores.at(3) << endl;
+    cout << test_scores.at(4) << endl;
+
+    cout << "Enter a new test score to add to our vector: " << endl;
+
+    int score_to_add = 0;
+    cin >> score_to_add;
+
+    test_scores.push_back(score_to_add);
+
+    cout << "Now we have: " << test_scores.size() << " elements in our vector" << endl;
+
+    cout << test_scores.at(0) << endl;
+    cout << test_scores.at(1) << endl;
+    cout << test_scores.at(2) << endl;
+    cout << test_scores.at(3) << endl;
+    cout << test_scores.at(4) << endl;
+    cout << test_scores.at(5) << endl;
 
     return 0;
 }
