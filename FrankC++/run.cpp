5c8d35b3e382a6c70c425ae4145cc2df1d8e5591
Jose Servin <Servinjose96@gmail.com>
2022-09-23 18:23:54 -0500

    starting logic flow and control statements

--- FrankC++/run.cpp
+++ FrankC++/run.cpp
@@ -4,9 +4,40 @@ using namespace std;
 
 int main()
 {
-    int number_of_toys = 12; // initialize
+    int grade = 0;
+    const int min = 0;
+    const int max = 100;
 
-    cout << number_of_toys << endl;
+    cout << "Enter your grade: " << endl;
+    cin >> grade;
 
-    return 0;
+    // first we check to see if the grade entered is inside the range of possible grades (between 0 and 100)
+    if (grade >= min && grade <= max)
+    {
+        if (grade < 60)
+        {
+            cout << "F" << endl;
+            cout << "Sorry, you will have to re-take the class. " << endl;
+        }
+        else if (grade >= 60 && grade < 70)
+        {
+            cout << "D" << endl;
+        }
+        else if (grade >= 70 && grade < 80)
+        {
+            cout << "C" << endl;
+        }
+        else if (grade >= 80 && grade < 90)
+        {
+            cout << "B" << endl;
+        }
+        else
+        {
+            cout << "A" << endl;
+        }
+    }
+    else
+    {
+        cout << grade << " is not between " << min << " and " << max << endl;
+    }
 }
