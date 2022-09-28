6b66eef0cf5d3e6febcc72fa24f8e85c0f6423af
Jose Servin <Servinjose96@gmail.com>
2022-09-28 15:50:07 -0500

    added conditional operator examples, moving onto for loops

--- FrankC++/run.cpp
+++ FrankC++/run.cpp
@@ -1,46 +1,21 @@
 #include <iostream>
 #include <vector>
-#include <iomanip>
 using namespace std;
 
 int main()
 {
-    int length = 0;
-    int width = 0;
-    int height = 0;
-    const int max_dimension = 10;
+    int num1 = 0;
+    int num2 = 0;
 
-    int volume = 0;
-    const double base_cost = 2.50;
-    double final_rate = 0;
-    double surcharge = 0;
+    cout << "Enter two numbers separated by a space: " << endl;
+    cin >> num1 >> num2;
 
-    cout << "Enter the length, width and height of your package: ";
-    cin >> length >> width >> height;
-
-    volume = length * width * height;
-
-    if (length <= max_dimension && width <= max_dimension && height <= max_dimension)
+    if (num1 != num2)
     {
-        if (volume > 100 && volume < 500)
-        {
-            surcharge = base_cost * .10;
-            final_rate = base_cost + surcharge;
-        }
-        else if (volume > 500)
-        {
-            surcharge = base_cost * .25;
-            final_rate = base_cost + surcharge;
-        }
-        else
-        {
-            final_rate = 2.50;
-        }
-        cout << fixed << setprecision(2);
-        cout << "Your final shipping rate is $" << final_rate << endl;
+        cout << "Largest: " << ((num1 > num2) ? num1 : num2) << endl;
+        cout << "Smallest: " << ((num1 < num2) ? num1 : num2) << endl;
     }
     else
-    {
-        cout << "Sorry, one of your dimensions is greater than 10 inches, we cannot ship." << endl;
-    }
+        (
+            cout << "The numbers are equal" << endl;
 }
