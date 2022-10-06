639015940390be0ef6ba8834501833a7268c10c2
Jose Servin <Servinjose96@gmail.com>
2022-10-05 20:30:25 -0500

    do while and continue vs break notes

--- FrankC++/run.cpp
+++ FrankC++/run.cpp
@@ -4,39 +4,16 @@ using namespace std;
 
 int main()
 {
-
-    // vector<int> test = {10, 20, 30, 40, 50, 110, 100};
-
-    // int index = 0;
-    // int count = 0;
-    // bool flag = false;
-    // while (index < test.size() && !flag)
-    // {
-    //     if (test[index] == 99)
-    //     {
-    //         int count = -1;
-    //         flag = true;
-    //         count += 1;
-    //     }
-    //     else
-    //     {
-    //         count += 1;
-    //         ++index;
-    //     }
-    // }
-    // cout << count << endl;
-    vector<int> vec = {10, 20, 30, 40, 50, 99, 100};
-    int count{0};
-    int index{0}; // See the Q/A forum for more about size_t
-                  // size_t is an unsigned int
-                  // you can replace size_t with int or unsigned int and it will work fine
-
-    while (index < vec.size() && vec.at(index) != 99)
+    cout << "Let's count from 1 to 10: " << endl;
+    for (int i = 1; i <= 10; ++i)
     {
-        ++count;
-        ++index;
+        if (i == 8)
+        {
+            break;
+        }
+        cout << i << endl;
     }
-    cout << count << endl;
 
+    cout << "Oops, there was an 8 in this loop! " << endl;
     return 0;
 }
